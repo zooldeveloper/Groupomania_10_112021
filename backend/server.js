a0ab43cecb0ec2401b8870b2c706d9709cb7b9d1
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-11 20:56:27 +0100

    file has been modefied a litte bit

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,12 @@
-const express = require('express');
-const mysql = require('mysql');
-const dotenv = require('dotenv');
-dotenv.config({ path: './.env'})
+const http = require('http');
 
-// Creates connection
-const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_NAME,
-});
+require('dotenv').config({ path: './config/.env' });
+require('./config/database');
 
-// Connect to our DB
-db.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySql connected...');
-	
-});
+const app = require('./app');
+app.set('port', process.env.PORT || 3000);
 
-const app = express();
-
-app.listen('3001', (req, res) => {
-    console.log('It\'s working')
-})
+const server = http.createServer(app);
 
 
-
-
-// const http = require('http')
-
-
-// const server = http.createServer((req, res) => {
-//     res.end('Server started...');
-//     console.log('Our server is working...')
-// });
-
-
-// server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000)

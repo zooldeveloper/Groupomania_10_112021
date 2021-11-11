090e4878901a98dddf0db32b780b6b59045c6d60
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-11 21:05:04 +0100

    " a user router file has been added" " "

---
+++ backend/routes/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+
+module.exports = router;

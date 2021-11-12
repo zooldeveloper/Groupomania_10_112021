486589796cd13209aa29867cc20ce5182d79a17a
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-12 16:08:55 +0100

    Headers have been added

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/user');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/api/auth', userRoutes);

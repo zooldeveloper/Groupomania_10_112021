5d86ec0453f21e525f83ed3112d61e68170c01c0
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-12 16:10:44 +0100

    Removed the connect method

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,9 @@
-
 const mysql = require('mysql');
 
 // Creates connection
-const db = mysql.createConnection({
+module.exports = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-}).connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log('MySql connected...');
-	
 });
-
-module.exports = db;

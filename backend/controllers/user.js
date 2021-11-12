1409032cf7886aca3c87fbeb4ad26e965884513a
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-12 16:15:09 +0100

    Modifications have been made

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,25 +1,45 @@
-require('dotenv').config({ path: '../config/.env' });
-const db = require('../config/database');
-const bcrypt = require('bcrypt');
+/** @format */
 
+require('dotenv').config({ path: '../config/.env' });
+let db = require('../config/database');
+let bcrypt = require('bcrypt');
+
+db.connect(err => {
+	if (err) {
+		console.log(err);
+	}
+	console.log('MySql is connected...!');
+});
 
 exports.signup = (req, res, next) => {
-    console.log(req.body)
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const email = req.body.email;
-    const password = req.body.password;
-    const passwordConfirm = req.body.passwordConfirm;
-    
-    db.query('INSERT INTO user (firstName, lastName, email, password, passwordConfirm) VALUES (?, ?, ?, ?, ?)',
-        [firstName, lastName, email, password, passwordConfirm],
-        (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).json({ err })
-            } else {
-                res.status(201).json({ message: 'User created!' })
-            }
-        });
-    //    res.send('It is working...!')
+	console.log(req.body);
+
+	bcrypt.hash(req.body.password, 8)
+        .then(hash => {
+            
+			bcrypt.hash(req.body.passwordConfirm, 8)
+                .then(hashConfirm => {
+                    
+					let firstName = req.body.firstName;
+					let lastName = req.body.lastName;
+					let email = req.body.email;
+					let password = hash;
+                    let passwordConfirm = hashConfirm;
+                    
+                    db.query('INSERT INTO user (firstName, lastName, email, password, passwordConfirm) VALUES (?, ?, ?, ?, ?)',
+                        [`${firstName}`,`${lastName}`,`${email}`,`${password}`,`${passwordConfirm}`],
+                        (err, result) => {
+                            if (err) {
+                                console.log(err);
+                                res.status(500).json({ err });
+                            } else {
+                                res.status(201).json({ message: 'User created!' });
+                            }
+                        }
+                    );
+                    
+				})
+				.catch(err => res.status(500).json({ error }));
+		})
+		.catch(err => res.status(500).json({ error }));
 };

961a7d32ee0b3af4a246c0e9ecc9ed71b1d6be88
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-12 20:12:06 +0100

    Login controller has been added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,8 +1,8 @@
 /** @format */
-
 require('dotenv').config({ path: '../config/.env' });
-let db = require('../config/database');
-let bcrypt = require('bcrypt');
+const db = require('../config/database');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 db.connect(err => {
 	if (err) {
@@ -12,7 +12,6 @@ db.connect(err => {
 });
 
 exports.signup = (req, res, next) => {
-	console.log(req.body);
 
 	bcrypt.hash(req.body.password, 8)
         .then(hash => {
@@ -42,4 +41,38 @@ exports.signup = (req, res, next) => {
 				.catch(err => res.status(500).json({ error }));
 		})
 		.catch(err => res.status(500).json({ error }));
+};
+
+
+exports.login = (req, res, next) => {
+
+    db.query(`SELECT * FROM user WHERE id = ${req.params.id}`,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+
+            result.forEach(user => {
+
+                if (req.body.email === user.email) {
+                    bcrypt.compare(req.body.password, user.password)
+                        .then(valid => {
+                            if (!valid) {
+                                return res.status(401).json({ Message: 'Password is incorrect!' })
+                            }
+                            res.status(200).json({
+                                userId: user.id,
+                                token: jwt.sign(
+                                    { userId: user.id },
+                                    'RANDOM_TOKEN_SECRET',
+                                    { expiresIn: '24' }
+                                )
+                            });
+                        })
+                        .catch(err => res.status(500).json({ err }))
+                }
+            });
+        }
+    );
+
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
+router.post('/login/:id', userCtrl.login);
 
 module.exports = router;

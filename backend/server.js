906db370e0afa722fcb1d648d1cea638d8e1a98f
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-12 20:16:59 +0100

    Modified server port

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,9 @@ require('dotenv').config({ path: './config/.env' });
 require('./config/database');
 
 const app = require('./app');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 const server = http.createServer(app);
 
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3001)

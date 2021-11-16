d75996fa001152cb6609b247ad9103b53160cf6b
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-16 22:37:30 +0100

    routes/user.js file code has been to routes/auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const userCtrl = require('../controllers/auth');
+const authCtrl = require('../controllers/auth');
 
-router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/signup', authCtrl.signup);
+router.post('/login', authCtrl.login);
 
 module.exports = router;

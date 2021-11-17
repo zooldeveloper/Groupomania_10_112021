3fd2287ec4543f92f3b590a2cf786232c5ff48f4
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-17 21:48:56 +0100

    mysql queries have been modified

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,14 +5,14 @@ const jwt = require('jsonwebtoken');
 
 
 // Post request controller 
-exports.addDataToUserProfile = (req, res) => {
+exports.addDataToUserProfile = (req, res) => { 
 	// Checks the body obj whether is empty or not
 	function isBodyEmpty(body) {
 		if (Object.keys(body).length !== 0) {
 			return true;
 		}
 		return false;
-    };
+	};
 	// Checks the file obj whether is empty or not
 	function isFileEmpty(file) {
 		if (file !== undefined) {
@@ -23,28 +23,28 @@ exports.addDataToUserProfile = (req, res) => {
 
 	if (isBodyEmpty(req.body) && isFileEmpty(req.file)) {
 		const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
-		db.query(`INSERT INTO user_profile (bio, imageUrl) VALUES (?, ?)`,[`${req.body.bio}`, `${imageUrl}`],
+		db.query(`UPDATE users SET bio = '${req.body.bio}', imageUrl = '${imageUrl}' WHERE id = ${req.params.id}`,
 			(err, result) => {
 				if (err) {
 					return res.status(500).json(err);
 				}
-				return res.status(201).json({ message: "Profile's data have been saved!" });
+				return res.status(201).json({ message: "User's profile's data have been saved!" });
 			}
 		);
     }
     else if (isBodyEmpty(req.body) && !isFileEmpty(req.file)) {
-		db.query(`INSERT INTO user_profile (bio) VALUES (?)`,[`${req.body.bio}`],
+		db.query(`UPDATE users SET bio = '${req.body.bio}' WHERE id = ${req.params.id}`,
 			(err, result) => {
 				if (err) {
 					return res.status(500).json(err);
 				}
-				return res.status(201).json({ message: "Profile's bio has been saved!" });
+				return res.status(201).json({ message: "User's profile's bio has been saved!" });
 			}
 		);
     }
     else if (!isBodyEmpty(req.body) && isFileEmpty(req.file)) {
 		const imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
-		db.query(`INSERT INTO user_profile (imageUrl) VALUES (?)`,[`${imageUrl}`],
+		db.query(`UPDATE users SET imageUrl = '${imageUrl}' WHERE id = ${req.params.id}`,
 			(err, result) => {
 				if (err) {
 					return res.status(500).json(err);
@@ -62,12 +62,12 @@ exports.addDataToUserProfile = (req, res) => {
 // Get request controller
 exports.getOneUser = (req, res) => {
 
-    db.query(`SELECT IF EXIST firstName, lastName, email  FROM user WHERE id = ?`, [req.params.id],
+    db.query(`SELECT id, firstName, lastName, email, bio, imageUrl  FROM users WHERE id = ?`, [req.params.id],
         (err, result) => {
             if (err) {
                 return res.status(500).json(err);
             }
-            return res.status(200).json(result);
+            res.status(200).json(result);
         }
-    );
+	);
 };

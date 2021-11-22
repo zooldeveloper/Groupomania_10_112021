1a5ca2e892995e4710caa637369f1a8305d84f7f
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-22 23:52:48 +0100

    Added the unlink function plus some improvements

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,14 +1,14 @@
 const db = require('../config/database');
+const fs = require('fs');
 
-
+// Post request controller
 exports.creatPost = (req, res) => {
-    
     const body = req.file ?
         {
             ...req.body,
             image_url: `${req.protocol}://${req.get('host')}/images/posts/${req.file.filename}`,
         } : { ...req.body };
-    
+    // Inserrs data into the posts table
     db.query(`INSERT INTO posts (textual_post, image_url, creation_date, user_id) VALUES (?, ?, ?, ?)`,
         [`${body.textual_post}`, `${body.image_url}`, new Date(), req.params.id],
         (err, result) => { 
@@ -20,8 +20,10 @@ exports.creatPost = (req, res) => {
     );
 };
 
-exports.getAllPosts = (req, res) => {
 
+// Get request controller for all posts
+exports.getAllPosts = (req, res) => {
+    // Gets all the data from posts & users tables based on conditions
     db.query(`SELECT * FROM posts JOIN users ON posts.user_id = users.id WHERE users.active = 'true'`,
         (err, result) => {
             if (err) {
@@ -37,14 +39,15 @@ exports.getAllPosts = (req, res) => {
     );
 };
 
+
+// Put request controller
 exports.modifiyPost = (req, res) => {
-    
     const body = req.file ?
         {
             ...req.body,
             image_url: `${req.protocol}://${req.get('host')}/images/posts/${req.file.filename}`,
         } : { ...req.body };
-    
+    // Updates posts table content
     db.query(`UPDATE posts SET textual_post = ?, image_url = ?, creation_date = ? WHERE post_id = ?`,
         [`${body.textual_post}`, `${body.image_url}`, new Date(), `${req.params.id}`],
         (err, result) => {
@@ -57,43 +60,38 @@ exports.modifiyPost = (req, res) => {
 
 };
 
-exports.deletePost = (req, res) => {
 
-    // const body = req.body.textualPostToDelelte && req.body.imagePostToDelelte ?
-    //     { ...req.body, } : req.body.textualPostToDelelte ?
-    //     { ...req.body, } : { ...req.body };
+// Delete request controller
+exports.deletePost = (req, res) => {
     
-    if(req.body.textualPostToDelelte && req.body.imagePostToDelelte) {
         // Selects all the data from the posts table whose id is equal to the id sent by the request
-        db.query(`SELECT * FROM posts WHERE post_id = ?`, req.params.id,
-            (err, result) => {
-                if (err) {
-                    console.log(err);
-                }
-                let deleted_posts = [];
-                for (let obj of result) {
-                    deleted_posts.push(obj);
-                }
-                // Inserts the deleted data into the deleted_posts table
-                db.query(`INSERT INTO deleted_posts (post_id, textual_post, image_url, creation_date, user_id) VALUES (?, ?, ?, ?, ?)`,
-                    [deleted_posts[0].post_id, `${deleted_posts[0].textual_post}`, `${deleted_posts[0].image_url}`, `${deleted_posts[0].creation_date}`, deleted_posts[0].user_id],
-                    (err, result) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        console.log(result)    
-                    }
-                );
+    db.query(`SELECT * FROM posts WHERE post_id = ?`, req.params.id,
+        (err, postResult) => {
+            if (err) {
+                return console.log(err)
+            };
+            let deleted_posts = [];
+            for (let obj of postResult) {
+                deleted_posts.push(obj);
             }
-        );
-        // Removes all the data from posts table where the id is equal to the id sent by the request
-        db.query(`DELETE FROM posts WHERE post_id = ?`, req.params.id,
-            (err, result) => {
-                if (err) {
-                    return res.status(500).json(err);
-                }
-                res.status(200).json(result);
-            }
-        );
-    }
+            // Inserts the deleted data into the deleted_posts table
+            db.query(`INSERT INTO deleted_posts (post_id, textual_post, image_url, creation_date, user_id) VALUES (?, ?, ?, ?, ?)`,
+                 [deleted_posts[0].post_id, `${deleted_posts[0].textual_post}`, `${deleted_posts[0].image_url}`, `${deleted_posts[0].creation_date}`, deleted_posts[0].user_id],
+                (err, deletedPostResult) => {
+                    if (err) {
+                        return console.log(err);
+                    };
+                    const filename = postResult[0].image_url.split('/posts/')[1];
+                    fs.unlink(`images/posts/${filename}`, () => {
+                        // Removes all the data from posts table where the id is equal to the id sent by the request
+                        db.query(`DELETE FROM posts WHERE post_id = ?`, req.params.id,
+                            (err, result) => {
+                                if (err) {
+                                    return res.status(500).json(err);
+                                }
+                                res.status(200).json(result);
+                            });
+                    });
+                });
+        });
 }; 

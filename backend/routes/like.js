aab89e05b4c6cf25a0be250a8b68bc57267cccd8
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-24 13:36:03 +0100

    Refactored a controller func name and removed the other routers

--- backend/routes/like.js
+++ backend/routes/like.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 const likeCtrl = require('../controllers/like');
 
-router.post('/', likeCtrl.createLike);
-router.get('/', likeCtrl.getLike); 
-router.put('/:id', likeCtrl.updateLike); 
-router.delete('/:id', likeCtrl.deleteLike); 
+router.post('/', likeCtrl.createAndUpdateLikes);
+router.get('/', likeCtrl.getLikesAndDislikes); 
 
 module.exports = router; 

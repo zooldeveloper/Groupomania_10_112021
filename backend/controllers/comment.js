be91ab882addd44124629d1f74a8f2ed01e880f4
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-11-24 18:52:50 +0100

    Defined the the requests controllers

---
+++ backend/controllers/comment.js
@@ -0,0 +1,24 @@
+const db = require('../config/database');
+
+// 
+exports.createComment = (req, res) => {
+    // res.send('This is a test')
+};
+
+
+// 
+exports.getComment = (req, res) => {
+    // res.send('This is a test 2')
+};
+
+
+// 
+exports.updateComment = (req, res) => {
+    // res.send('This is a test 3')
+};
+
+
+// 
+exports.deleteComment = (req, res) => {
+    // res.send('This is a test 4')
+};

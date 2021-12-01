b3f5c029b5898f9554ea08c6ec43bfcc45d86b3c
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-01 18:33:07 +0100

    First commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Signup/>
+</template>
+
+<script>
+import Signup from './components/Signup.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Signup
+  }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ frontend/src/components/Signup.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="banner">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  props: {
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .banner {
+    width: 100%;
+    height: 100vh;
+    background-image: url("../assets/images/banner1.jpeg");
+}
+</style>

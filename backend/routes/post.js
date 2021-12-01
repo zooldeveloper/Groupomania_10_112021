0b7fab7e9ba0f4476c2448a51292b2f9a1fda62c
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-01 14:46:26 +0100

    Authentiction has been added to the posts route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const postCtrl = require('../controllers/post');
 const multer = require('../middleware/multer-config');
 const upload = multer.single('post_image');
+const authCheck = require('../middleware/auth-check.js');
 
-router.post('/:id', upload, postCtrl.creatPost);
+router.post('/:id', authCheck, upload, postCtrl.creatPost);
 router.get('/', postCtrl.getAllPosts);
-router.patch('/:id', upload, postCtrl.modifiyPost);
-router.delete('/:id', upload, postCtrl.deletePost);
+router.patch('/:id', authCheck, upload, postCtrl.modifiyPost);
+router.delete('/:id', authCheck, upload, postCtrl.deletePost);
 
 module.exports = router;  

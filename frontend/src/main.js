1299f9127e89a5629b919237386b157c896dda61
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-03 18:34:32 +0100

    Vue router has been added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+
+// Vue.config.productionTip = false
+
+createApp(App)
+    .use(router)
+    .component('font-awesome-icon', FontAwesomeIcon)
+    .mount('#app')

558122d9f1eb15cbf35c5ba30754fa570953ab5d
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-09 11:07:36 +0100

    Now the responseErrorMessage appears with only one click"

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -202,6 +202,9 @@ export default {
               email: this.email,
               password: this.password
             })
+            setTimeout(() => {
+              this.submitForm()
+              }, 2000)
           }
       },
       isUserValid(firstname) {

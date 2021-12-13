0399265488c1aca17657abe38ee636c09c620f79
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-13 23:32:32 +0100

    Hearder section is moved to the Header component

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,26 +1,15 @@
 <template>
-    <header>
-        <nav>
-            <div class="logo">
-              <a href=""><img src="../assets/images/icon-left-font-monochrome-gray.svg" alt="company logo"></a>
-            </div>
-            <div class="navbar">
-                <ul>
-                    <li><router-link to="/"> <font-awesome-icon icon='home' color='#71838F' size="lg"/> </router-link></li>
-                    <li><router-link to="/notif"> <font-awesome-icon icon='bell' color='#71838F' size="lg"/> </router-link></li>
-                    <li><router-link to="/account"> <font-awesome-icon icon='user-alt' color='#71838F' size="lg"/> </router-link></li>
-                    <li><button><font-awesome-icon icon='power-off'/> </button></li>
-                </ul> 
-            </div>
-        </nav>
-    </header>
-  
+    <Header/>
 </template>
 
 <script>
+import Header from '../components/Header.vue'
 
 export default {
   name: 'Home',
+  components: {
+    Header,
+  },
   mounted() {
     const token = document.cookie;
     if (!token) {
@@ -32,23 +21,5 @@ export default {
 
 
 <style lang="scss" scoped>
-   @import '@/assets/sass/variables.scss';
-  @import '@/assets/sass/mixins.scss';
-
-  nav {
-    width: 100%;
-    height: 80px;
-    background: linear-gradient(to bottom left, lighten($primary_color, $percentage1), lighten($secondary_color, $percentage1));
-    @include flexbox(space-around);
-    .navbar {
-      width: 30%;
-      ul {
-         @include flexbox(space-around);
-         list-style: none;
-      }
-    }
-  }
-  img{
-    width: 250px;
-  }
+  
 </style>

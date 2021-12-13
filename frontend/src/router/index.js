fb5faf51914616bfdcdf66a4d32cbc554e47efc4
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-13 22:10:04 +0100

    An about router has been rchanged to  account

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,12 +9,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/account',
+    name: 'Account',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Account.vue')
   },
   {
     path: '/entry',

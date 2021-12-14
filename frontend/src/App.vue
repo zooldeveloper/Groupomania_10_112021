1bcfea99b780111e13ba5ad46ddf9c3fa4a79986
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-14 23:02:45 +0100

    A body & #app selectors are added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <template>
-  <div id="nav">
     <router-link to="/"></router-link> 
     <router-link to="/entry"></router-link>
-  </div>
   <router-view/>
 </template>
 
@@ -18,5 +16,15 @@ export default {
 </script>
 
 <style lang="scss">
-
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 </style>

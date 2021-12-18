20549df93d499d7eaa71721c5ab51d04bf73f0f3
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-18 18:24:21 +0100

    User profile has been added statically

---
+++ frontend/src/views/Account.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="about">
+      <Header/>
+      <main>
+          <section id="userinfo">
+              <div class="userinfo__wrapper">
+                  <div class="userinfo__user userinfo__user-border">
+                      <h1>User name</h1>
+                      <img src="../assets/images/banner1.jpeg" alt="user image"/>
+                      <p>E-email: mack@gmail.com</p>
+                  </div>
+                  <div class="userinfo__user">
+                      <h2>Bio :</h2>
+                      <p>Lorem ipsum dolor sit amet. Id enim rerum et consectetur eaque non iusto officia qui quia ut praesentium harum qui nulla minima !</p>
+                      <form class="userinfo__bioform">
+                          <textarea class="userinfo__biotextarea" name="" id="" placeholder="Your bio!"></textarea>
+                          <button class="userinfo__btn" type="submit"><font-awesome-icon icon='paper-plane' color='#76c8d3' size="lg"/></button>
+                      </form>
+                  </div>
+              </div>
+          </section>
+          <section></section>
+      </main>
+  </div>
+</template>
+
+
+<script>
+import Header from '../components/Header.vue'
+import Form from '../components/Form.vue'
+export default {
+  name: 'Home',
+  components: {
+    Header, Form
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/sass/variables.scss';
+  @import '@/assets/sass/mixins.scss';
+
+
+
+
+  .about {
+    margin-top: 130px;
+    
+    #userinfo {
+        padding: 30px 45px;
+        box-shadow: 0px 5px 15px $border-color;
+      .userinfo {
+        &__wrapper {
+          @include flexbox(center, start);
+          border-top: 1px solid $border-color;
+          border-bottom: 1px solid $border-color;
+          text-align: left;
+        }
+        &__user {
+           @include flexbox(space-between);
+            flex-direction: column;
+            width: 45%;
+            img {
+            width: 180px;
+            height: 200px;
+            object-fit: cover;
+            text-align: left
+          }
+        }
+        &__bioform {
+          position: relative;
+          margin-top: 10px;
+          width: 100%;
+        }
+        &__biotextarea {
+          width: 100%;
+          height: 50px;
+          padding: 10px 20px 0 10px;
+          border-radius: 25px;
+          resize: none;
+          box-sizing: border-box;
+        }
+        &__btn {
+          @include btn;
+          right: 15px;
+          top: 12px;
+        }
+        @media  screen and (max-width:768px) {
+          &__wrapper {
+            flex-direction: column;
+            align-items: center;
+          }
+          &__user {
+             width: 75%;
+          }
+          &__user-border {
+            border-bottom: 1px solid $border-color;
+          }
+           &__bioform {
+             margin-bottom: 20px;
+           }
+        }
+      }  
+    }
+  }
+      
+</style>

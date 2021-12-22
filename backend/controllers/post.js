b356b0aec116596aa3b78a611f0a283368263839
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-22 19:28:38 +0100

    Replaced the Date object with a new date format

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -8,9 +8,13 @@ exports.creatPost = (req, res) => {
             ...req.body,
             image_url: `${req.protocol}://${req.get('host')}/images/posts/${req.file.filename}`,
         } : { ...req.body };
+    
+    const event = new Date(Date.now());
+    const options = { hour: '2-digit', minute: '2-digit', year: 'numeric', month: 'short', day: 'numeric' };
+    const timestamp = event.toLocaleDateString('en-FR', options)
     // Inserrs data into the posts table
     db.query(`INSERT INTO posts (textual_post, image_url, creation_date, user_id) VALUES (?, ?, ?, ?)`,
-        [`${body.textual_post}`, `${body.image_url}`, new Date(), req.params.id],
+        [body.textual_post, body.image_url, timestamp, req.params.id],
         (err, result) => { 
             if (err) {
                 return res.status(500).json(err);

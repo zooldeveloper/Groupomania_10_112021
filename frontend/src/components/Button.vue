79d7b1d7a5f232456174b2f2c96fa263d537430b
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-25 12:33:25 +0100

    Button component is added

---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,41 @@
+<template>
+    <button :class="classes">{{ name }}</button>
+</template>
+
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        classes: {
+            type: String,
+            required: true,
+        },
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+    @import '@/assets/sass/variables.scss';
+
+    .btn {
+        width: 80px;
+        height: 30px;
+        border-radius: 10px;
+        border: none;
+        background-color: $primary-color;
+        color: $quaternary-color;
+        font-weight: bold;
+    }
+    .custom-btn {
+        background-color: $secondary-color;
+    }
+     @media screen and (max-width:768px) {
+        .btn { width: 100px; }
+     }
+</style>

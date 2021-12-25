3d156d654ef5ea31b2d5a6e97825a5eb6bf475e8
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2021-12-26 00:00:46 +0100

    A delete confirm component is added

---
+++ frontend/src/components/DeleteConfirm.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="delete" v-if="confirmDelete">
+        <p>Delete post</p>
+        <div class="delete__cancel-confirm">
+             <Button name="Cancel" classes="btn"/>
+             <Button name="Confirm" classes="btn custom-btn"/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import Button from './Button.vue'
+
+export default {
+    name: 'DeleteConfirm',
+    components: {
+        Button,
+    },
+    props: {
+        confirmDelete:{
+            type: Boolean,
+        } 
+    },
+}
+</script>
+
+
+<style lang="scss" scoped>
+    @import '@/assets/sass/variables.scss';
+    @import '@/assets/sass/mixins.scss';
+
+    .delete {
+        position: absolute;
+        width: 230px;
+        height: 100px !important;
+        z-index: 2;
+        top: -10px;
+        left: -190px;
+        background-color: $quaternary-color;
+        box-shadow: 0px 5px 15px $border-color;
+        text-align: center;
+        p {
+            font-size: 1.2rem !important;
+            margin-bottom: 10px;
+        }
+        &__cancel-confirm {
+            @include flexbox(space-around);
+            color: #000;
+        }
+    }
+        
+</style>

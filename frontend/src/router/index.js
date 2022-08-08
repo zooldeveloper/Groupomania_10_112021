01c6dd25716602e6d285252ffe6dc957f5301eeb
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-08-08 21:13:01 +0200

    Profile router has been added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,10 +14,7 @@ const routes = [
 		// route level code-splitting
 		// this generates a separate chunk (about.[hash].js) for this route
 		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(
-				/* webpackChunkName: "account" */ '../views/Account.vue'
-			),
+		component: () => import(/* webpackChunkName: "account" */ '../views/Account.vue'),
 	},
 	{
 		path: '/users',
@@ -28,6 +25,11 @@ const routes = [
 		path: '/notification',
 		name: 'Notification',
 		component: () => import('../views/Notification.vue'),
+  },
+	{
+		path: '/profile',
+		name: 'Profile',
+		component: () => import('../views/Profile.vue'),
 	},
 	{
 		path: '/entry',

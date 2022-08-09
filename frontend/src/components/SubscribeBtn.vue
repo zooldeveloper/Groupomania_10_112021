a1d865c2cb5da02dd02a4780855577481bced3c4
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-08-09 23:41:37 +0200

    Added SubscribeBtn component

---
+++ frontend/src/components/SubscribeBtn.vue
@@ -0,0 +1,40 @@
+<template>
+      <button>
+            <font-awesome-icon :icon="icon" :color="color" :size="size" @click="onSubscribe"/>
+      </button>
+</template>
+
+<script>
+      export default {
+            name: 'SubscribeBtn',
+            props: {
+                  icon: {
+                        type: Boolean,
+                        required: true,
+                  },
+                  color: {
+                        type: String,
+                        required: true,
+                   },
+                   size: {
+                        type: String,
+                        required: true,
+                  },
+            },
+            methods: {
+                  onSubscribe() {
+                        this.$emit('trigger-on-subscribe')
+                  }
+            }
+                   
+      }
+</script>
+
+<style lang="scss" scoped>
+      button {
+            border: none;
+            background: none;
+            cursor: pointer;
+      }
+
+</style>

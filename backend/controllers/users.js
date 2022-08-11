1e821465ea2ee056e987e75523e23c96e2fd1e62
Mukhtar SULAIMAN <mukhtar.sulaiman.pro@gmail.com>
2022-08-11 23:06:39 +0200

    Modified sql quety

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,7 +5,9 @@ exports.getAllUsers = (req, res) => {
 
 	// Selects all users and subscribers data that matches the conditions
 	db.query(`SELECT users.id, users.firstName, users.lastName, users.email, users.imageUrl, users.bio, users.jobTitle, users.isAdmin,
-			(SELECT COALESCE(COUNT(subscribed_user),0) FROM subscribers WHERE profile_owner = users.id AND subscriber_status = 'true') AS subscribersNum FROM subscribers
+			(SELECT COALESCE(COUNT(subscribed_user),0) FROM subscribers WHERE profile_owner = users.id AND subscriber_status = 'true') AS subscribersNum,
+			(SELECT GROUP_CONCAT(subscribed_user) FROM subscribers WHERE profile_owner = users.id AND subscriber_status = 'true') AS subscribedUsers 
+			FROM subscribers
 			JOIN users
 			WHERE users.active = 'true'
 			GROUP BY users.id
